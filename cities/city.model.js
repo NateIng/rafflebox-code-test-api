890aa3b3c0c8cbb1554434265e70c28de6d5a03e
nyro7 <nyro7.nate3@gmail.com>
2022-02-13 21:24:08 -0400

    id fix

--- cities/city.model.js
+++ cities/city.model.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    id: { type: String, unique: true, required: true },
     name: { type: String, required: true },
     temperature: { type: Number, required: true },
     date: { type: Date, default: Date.now },
